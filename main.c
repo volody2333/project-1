6d6f93f6e71a738bc66ea52b9332b7f5092a0549
volody2333 <volody2333@mail.ru>
2026-01-17 14:16:36 +1000

    Upfate

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
+#pragma hdrstop
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>
+#pragma argsused
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
- }
-
+int main (int argc, char* argv [])
+{char i;
+ int j;
+ float f, f1, f2;
+ long double d, d1, d2;
+ printf("Example for first lection \n\n");
+ i=1;
+ j=-123456789;
+ 
+ printf("i= %d \n",i);
+ printf("j= %d \n",j);
+ 
+ f1=3.25;
+ f2=4.75;
+ f=f1+f2;
+ 
+ printf("f= %f \n",f);
+ 
+ d1=3.25E00;
+ d2=4.75E00;
+ d=d2-d1;
+ 
+ printf("d= %LE \n\n",d);
+ //printf("d= %LF \n\n",d); 
+ 
+ printf("OK");
+ getch();
+        return 0;
+}
