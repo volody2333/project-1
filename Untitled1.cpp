ea3d54f0cbecd6577e5ce79393fc9cc9a21de5fd
volody2333 <volody2333@mail.ru>
2026-01-18 19:45:50 +1000

    update

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main() {
+
+	int a = 5, b = 10, res;
+	a *=5;
+	a--;
+	
+	res = a + b;
+	
+	printf("Variable: %d + %d = %d\n", a, b, res);
+	
+	
+	
     printf("Hello!\n");
-    printf ("2021");
+    printf ("2026\n");
     getchar();
     return 0;
 }
