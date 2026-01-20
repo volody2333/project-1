6a733c2169951e43bbafc4d7abe038842138ba50
volody2333 <volody2333@mail.ru>
2026-01-20 22:21:20 +1000

    20.01.2026

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,19 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
+#include <stdbool.h>
 
-	int a = 5, b = 10, res;
-	a *=5;
-	a--;
+int main() {
 	
-	res = a + b;
+	float x,  y, res;
+	scanf("%f", &x); 
+	scanf("%f", &y);
 	
-	printf("Variable: %d + %d = %d\n", a, b, res);
+	res = x / y; 
+	
+	 
+	 
+	//const short A = 0;
+	// long int
+	// printf - ld
+	
+	//res = q / w;
+	printf("Result: %.2f\n", res);
+	
+	//char s ='S';
+	//printf("%c\n", s);
 	
 	
 	
-    printf("Hello!\n");
-    printf ("2026\n");
+	
+   	//bool Car = true;
+	//bool RedCar = foul;
+	
+    // до -32 768
+	//short s = 500;
+	
+    //unsigned x = 5;
+    
+	//int a = 5, b = 10, res;
+	
+	// 0 - 4 млн
+	//unsigned int x1 = 6;
+	
+	//long x2 = 5;
+	//long long x3 = 6;
+	
+	//a *= 5;
+	//a--;
+	//b++;
+	
+	//float x5 = 4.545343636f;
+	//double x6 = 5.48538548345853;
+	
+	//char sym = 'Q';
+	// Запись символа.
+	
+	
+	
+	//res = a + b;
+	
+	//printf("Variable: %d + %d = %d\n", a, b, res);
+	
+	
+	
+    //printf("Hello!\n");
+    //printf ("2026\n");
     getchar();
     return 0;
 }
