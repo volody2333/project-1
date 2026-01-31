deb89f379e79f200eb5891db9ec8c576064a6e67
volody2333 <volody2333@mail.ru>
2026-01-31 19:13:43 +1000

    Изучил тему Условные опеторы

---
+++ main.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() {
+	
+	//Услов. опер. свич кейс более точен, ёмок.
+//    int x = 15;
+//    switch(x) {
+//        case 10:
+//		 printf("10");
+//         break;
+//        case 15:
+//		 printf("15");
+//		 break;
+//		 
+//		 default:
+//		   printf("Error\n")	
+//	}
+//    
+    
+	// УСЛОВНЕЫ ОПЕРАТОРЫ (и\ или \ не правда\)
+//	int x = 20, y = 5;
+//	        
+//	if((x == 10 && y < 7 ) || x == 20)
+//		printf("Yes\n");
+//	
+//	else if(x > 100) {
+//		if(x == 105)
+//		printf("X is equl 105\n");
+//		printf("X is greater 100\n");
+//	}
+//	else if(x < 5) 
+//		printf("X is less than 5\n");
+//	
+//	else 
+//		printf("No\n");
+//	
+//	
+//	bool isHasCar = true;
+//	if(isHasCar == true)
+//	printf("Yes\n");
+//	else
+//	printf("No\n");
+//	
+//	return 0;
+//}
+	
+	
+	
+	
+	
+	
+	
+	//СОЗДАНИЕ МАССИВА
+//	int arr[] = {5, 7 ,2 , 56, 34};
+//	arr[0] = 56;
+//	printf("%d\n", arr[0]);
+//	printf("%d\n", arr[2]);
+//	
+//	float numbers[3];
+//	numbers[0] = 5.4f;
+//	numbers[1] = 4.4f;
+//	numbers[2] = 67.34f;
+//	
+//	char word[] = {'S', 'o', 'm', 'e'};
+//	char words[] = "Hello Word!";
+//	
+//	words[1] = 'G';
+//	printf("%s\n", words);
+//	printf("%c\n", words[0]);
+//	
+//	int array[3][4] = {
+//	  {4, 5, 6, 5},
+//	  {7, 234, 4, 6},
+//	  {4, 678, 6, 7}
+//	
+//	};
+//	array[1][1] = 5;
+//	printf("%d\n", array[0][0]);
+//	
+//	//getchar();
+//    return 0;
+	
+	
+	//ПЕРЕМЕННЫЕ
+	
+	//float x,  y, res;
+	//scanf("%f", &x); 
+	//scanf("%f", &y);
+	
+	//res = x / y; 
+	
+	 
+	 
+	//const short A = 0;
+	// long int
+	// printf - ld
+	
+	//res = q / w;
+	//printf("Result: %.2f\n", res);
+	
+	//char s ='S';
+	//printf("%c\n", s);
+	//bool Car = true;
+	//bool RedCar = foul;
+	
+    // до -32 768
+	//short s = 500;
+	
+    //unsigned x = 5;
+    
+	//int a = 5, b = 10, res;
+	
+	// 0 - 4 млн
+	//unsigned int x1 = 6;
+	
+	//long x2 = 5;
+	//long long x3 = 6;
+	
+	//a *= 5;
+	//a--;
+	//b++;
+	
+	//float x5 = 4.545343636f;
+	//double x6 = 5.48538548345853;
+	
+	//char sym = 'Q';
+	// Запись символа.
+	
+	
+	
+	//res = a + b;
+	
+	//printf("Variable: %d + %d = %d\n", a, b, res);
+	
+	
+	
+    //printf("Hello!\n");
+    //printf ("2026\n");
+    
+
+
